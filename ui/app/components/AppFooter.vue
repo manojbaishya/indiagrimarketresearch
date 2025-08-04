9bf6f37d0ac947557e7445ffa395d8d16928b8ac
Manoj Baishya <28330014+manojbaishya@users.noreply.github.com>
2025-08-04 16:25:28 +0530

    fix theme

--- ui/app/components/AppFooter.vue
+++ ui/app/components/AppFooter.vue
@@ -1,3 +1,17 @@
 <template>
-    Footer Bar
+    <div id="footer">
+        <span>Privacy Policy</span>
+        <span>Terms Of Use</span>
+        <span>© 2022-2024 indiagrimarketresearch.com</span>
+        <span>Data Source</span>
+    </div>
 </template>
+
+<style lang="scss" scoped>
+
+#footer {
+    display: flex;
+    justify-content: space-around;
+}
+
+</style>
--- ui/app/components/AppHeader.vue
+++ ui/app/components/AppHeader.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
 import { navigationMenuTriggerStyle } from '@/components/ui/navigation-menu';
 import type { NavigationData } from '@/utils/navigation';
+import { Icon } from '@iconify/vue'
 
 defineProps<{ links: NavigationData }>()
+
+const colorMode = useColorMode();
 </script>
 
 <template>
-    <NavigationMenu>
+    <NavigationMenu disableHoverTrigger>
         <NavigationMenuList>
             <NavigationMenuItem v-for="page in links.pages" :key="page.meta.id">
                 <div v-if="page.hasChildren">
-                    <NavigationMenuTrigger>{{ page.meta.name }}</NavigationMenuTrigger>
+                    <NavigationMenuTrigger>{{ page.meta.name }}
+                    </NavigationMenuTrigger>
                     <NavigationMenuContent>
                         <NuxtLink v-for="link in page.children" v-slot="{ isActive, href, navigate }" :key="link.id"
                             :to="link.target" custom>
@@ -32,4 +36,27 @@ defineProps<{ links: NavigationData }>()
             </NavigationMenuItem>
         </NavigationMenuList>
     </NavigationMenu>
+
+    <DropdownMenu>
+        <DropdownMenuTrigger as-child>
+            <Button variant="outline">
+                <Icon icon="radix-icons:moon"
+                    class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Icon icon="radix-icons:sun"
+                    class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span class="sr-only">Toggle theme</span>
+            </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+            <DropdownMenuItem @click="colorMode.preference = 'light'">
+                Light
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="colorMode.preference = 'dark'">
+                Dark
+            </DropdownMenuItem>
+            <DropdownMenuItem @click="colorMode.preference = 'system'">
+                System
+            </DropdownMenuItem>
+        </DropdownMenuContent>
+    </DropdownMenu>
 </template>
